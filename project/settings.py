f91830101c2db87146edf63d07f57bba9684917e
kim-juwan <juwan92@naver.com>
2019-12-11 22:57:25 +0900

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -74,17 +74,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.oracle',
+#         'NAME':'xe',
+#         'USER':'admin',
+#         'PASSWORD':'1234',
+#         'HOST':'192.168.99.100',
+#         'PORT':'32764',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME':'xe',
-        'USER':'admin',
-        'PASSWORD':'1234',
-        'HOST':'192.168.99.100',
-        'PORT':'32764',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -108,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
