a27c8706ea71165e9e376eb47d0840fe9d1fbbb0
kim-juwan <juwan92@naver.com>
2019-12-11 20:35:55 +0900

    Summary

---
+++ rest/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views   # 현재 패키지에서 views 모듈을 가져옴
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('map',views.map,name='map'),
+]
