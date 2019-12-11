3c32cd88ef5be6bb70c62fc435c49780545ee76f
kim-juwan <58773699+kim-juwan@users.noreply.github.com>
2019-12-11 20:39:46 +0900

    Add files via upload

---
+++ views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import cx_Oracle as oci
+from django.db import connection
+# Create your views here.
+
+
+def index(request):
+    return render(request,'rest/index.html')
+
+def map(request):
+    return render(request,'rest/map.html')
