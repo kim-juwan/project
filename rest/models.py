c8b206c80478d962531d58b6afa3f85807fcc30a
kim-juwan <juwan92@naver.com>
2019-12-12 16:06:25 +0900

    주완

--- rest/models.py
+++ rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import cx_Oracle as oci
 # Create your models here.
+from django.db import connection
+
+
+class database():
+    def select():
+        cursor = connection.cursor()
+        select_sql = 'SELECT * FROM REST ORDER BY ID DESC'
+        cursor.execute(select_sql)
+        data = cursor.fetchall()
+        return data
