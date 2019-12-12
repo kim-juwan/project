316252a59eea9212aade39b5d685cc7211e7b6a7
Jangu0 <58797501+Jangu0@users.noreply.github.com>
2019-12-12 15:47:02 +0900

    ㅇ

--- rest/urls.py
+++ rest/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views   # 현재 패키지에서 views 모듈을 가져옴
 urlpatterns = [
     path('index', views.Index, name='index'),
-    path('home',views.Home, name='home')
+    path('home',views.Home, name='home'),
     path('base',views.Base,name='base'),
     path('search',views.Search,name='search'),
     path('detail',views.Detail,name='detail'),
