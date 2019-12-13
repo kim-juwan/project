51a6b65e21f886d7af732e6d9257f9718b2bbc44
kim-juwan <juwan92@naver.com>
2019-12-13 13:06:57 +0900

    주완

--- rest/models.py
+++ rest/models.py
@@ -2,21 +2,31 @@ from django.db import models
 import cx_Oracle as oci
 # Create your models here.
 from django.db import connection
-
+import random
 
 class Database():
     ## 오름차순 정렬로 데이터 베이스에서 가져오기
-    def Select_Desc():
-        cursor = connection.cursor()
+    def __init__(self):
+        self.cursor = connection.cursor()
+
+    def Select_Desc(self):
         select_sql = 'SELECT * FROM REST ORDER BY ID DESC'
-        cursor.execute(select_sql)
-        data = cursor.fetchall()
-        return data
+        self.cursor.execute(select_sql)
+        result_data = self.cursor.fetchall()
+        return result_data
     ## 내림차순 정렬로 데이터 베이스에서 가져오기
-    def Select_Asc():
-        cursor = connection.cursor()
+    def Select_Asc(self):
         select_sql = 'SELECT * FROM REST ORDER BY ID ASC'
-        cursor.execute(select_sql)
-        data = cursor.fetchall()
-        return data
-    
+        self.cursor.execute(select_sql)
+        result_data = self.cursor.fetchall()
+        return result_data
+    def Select_Search(self,search_word):
+        search_sql="SELECT * FROM REST WHERE NAME LIKE '%'|| :text ||'%' ORDER BY DIST ASC"
+        self.cursor.execute(search_sql,text=search_word)
+        result_data = self.cursor.fetchall()
+        return result_data
+
+class Func():
+    def randnum(self,data,n):
+        sample_data = random.sample(data,n)
+        return sample_data
