c0408c6a4cfba9afc7f6aac9a58701f0c439a7fa
kim-juwan <juwan92@naver.com>
2019-12-16 11:20:54 +0900

    주완

--- rest/models.py
+++ rest/models.py
@@ -39,6 +39,18 @@ class Database():
         self.cursor.execute(search_sql)
         result_data = self.cursor.fetchall()
         return result_data
+    def Select_One(self,idno):
+        select_sql = f'SELECT * FROM REST WHERE ID={idno}'
+        self.cursor.execute(select_sql)
+        result_data = self.cursor.fetchone()
+        return result_data
+    def Search_Cate(self,no,row,cate):
+        search_sql = f"SELECT * FROM (SELECT * FROM REST WHERE CATE LIKE (\'%{cate[0]}%\' OR \'%{cate[1]}%\' OR \'%{cate[2]}%\' OR \'%{cate[3]}%\' OR \'%{cate[4]}%\' OR \'%{cate[5]}%\' OR \'%{cate[6]}%\' OR \'%{cate[7]}%\' OR \'%{cate[8]}%\' OR \'%{cate[9]}%\' OR \'%{cate[10]}%\' OR \'%{cate[11]}%\' OR \'%{cate[12]}%\' OR \'%{cate[13]}%\' OR \'%{cate[14]}%\') WHERE (DIST < {cate[15]}) "
+        self.cursor.execute(search_sql)
+        for i in range(no):
+            result_data = self.cursor.fetchmany(row)
+        return result_data
+    
 class Func(Database):
     def __init__(self):
         pass
@@ -56,7 +68,7 @@ class Func(Database):
         
         
         page = leng // 10 + 1
-        page_1 = leng % 10
+        
         
         for i in range(1,page+1):
             if ((10*(i-1))+1) <= no <= ((10*(i))+1):
@@ -65,10 +77,7 @@ class Func(Database):
             ran10 = range((((page-1)*10)+1),leng+1)
         return [ran10,leng]
     def Image_Data(self,data):
-        image = []
         new_data = []
-        
-
         for img in data:
             name = img[2]
             addr = img[1]
@@ -90,4 +99,12 @@ class Func(Database):
             
             new_data.append(img2)
         return new_data
-        
+    def Image_Encoder(self,data):
+        if data[9]:
+            idata = data[9].read()  
+            image = b64encode(idata).decode("utf-8")
+        else :
+            file = open("static/icon/sashimi.png","rb")
+            idata = file.read()
+            image = b64encode(idata).decode("utf-8")
+        return image
